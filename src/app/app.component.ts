38feade7c69564b88c6baaacf521b8f49ea79d5c
Rayaan Meeran <rayaannmeeran@gmail.com>
2019-03-22 19:28:23 -0400

    Mostly finshed the lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,9 @@ export class AppComponent {
   title = 'TODO APP';
   todoList = todos.todo;
 
-  /*
-  todoList = [
-    {
-      task: "Buy Oranges",
-      complete: false,
-      description: "Buy 6 oranges from the store."
-    },
-    {
-      task: "Buy Grapes",
-      complete: false,
-      description: "Buy a pack of grapes from the store."
-    },
-    {
-      task: "Buy Apples",
-      complete: false,
-      description: "Buy 3 apples from the store."
-    }
-  ]*/
+  incompleteList = this.todoList;
+  completedList = [];
+
 
   addTodo(title, desc) {
     let duplicate = this.checkDuplicate(title);
@@ -35,7 +20,7 @@ export class AppComponent {
       confirm("You already have a Todo named " + title + ".");
     }
     else if(this.isEmpty(title) === false){
-        this.todoList.push({
+        this.incompleteList.push({
         task: title,
         complete: false,
         description: desc,
@@ -44,10 +29,20 @@ export class AppComponent {
     }
   }
 
-  delete(data) {
-    for(let i = 0; i < this.todoList.length; i++) {
-      if(data.task === this.todoList[i].task){
-        this.todoList.splice(i, 1);
+  deleteIncomplete(data) {
+    for(let i = 0; i < this.incompleteList.length; i++) {
+      if(data.task === this.incompleteList[i].task){
+        this.incompleteList.splice(i, 1);
+        console.log("Deleted.");
+        break;
+      }
+    }
+  }
+
+  deleteComplete(data) {
+    for(let i = 0; i < this.completedList.length; i++) {
+      if(data.task === this.completedList[i].task){
+        this.completedList.splice(i, 1);
         console.log("Deleted.");
         break;
       }
@@ -55,8 +50,8 @@ export class AppComponent {
   }
 
   checkDuplicate(titleInput) {
-    for(let i = 0; i < this.todoList.length; i++) {
-      if(titleInput === this.todoList[i].task){
+    for(let i = 0; i < this.incompleteList.length; i++) {
+      if(titleInput === this.incompleteList[i].task){
         return true;
       } 
     }
@@ -66,7 +61,8 @@ export class AppComponent {
   isEmpty(str) {
     return str === null || str.match(/^ *$/) !== null;
   }
-
+ 
+  /*
   setNotDone(data) { 
     for(let i = 0; i < this.todoList.length; i++) {
       if(data.task === this.todoList[i].task){
@@ -75,16 +71,48 @@ export class AppComponent {
         break;
       }
     }
-  }
+  }*/
 
   setDone(data) { 
-    for(let i = 0; i < this.todoList.length; i++) {
-      if(data.task === this.todoList[i].task){
-        this.todoList[i].complete = true;
+    for(let i = 0; i < this.incompleteList.length; i++) {
+      if(data.task === this.incompleteList[i].task){
+        this.incompleteList[i].complete = true;
+        this.completedList.push(this.incompleteList[i]);
+        this.incompleteList.splice(i, 1);
         console.log("Task Done.");
         break;
       }
     }
   }
 
+  checkCompleted(data) {
+    if(data.complete === true) {
+
+    }
+  }
+
+  onDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  onDrop(e) {
+    e.preventDefault();
+    
+    if (e.dataTransfer.items) {
+          var file = e.dataTransfer.items[0].getAsFile();
+
+          let fileReader = new FileReader();
+          fileReader.onload = (e) => {
+            let newJSONString = fileReader.result.toString(); // Changing the file data to a String
+            console.log(newJSONString);
+            let newJSONObj = JSON.parse(newJSONString); // Parsing the String 
+            console.log(newJSONObj);
+            this.incompleteList = newJSONObj.todo;
+          }
+          fileReader.readAsText(file);
+    } else {
+      console.error("There was an error with the file drop.");
+    }
+  }
 }
